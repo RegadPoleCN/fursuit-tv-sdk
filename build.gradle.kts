55c4c00320b46ccaf343242e0f351cf5ebc05196
RegadPoleCN <bilibilipopojun@hotmail.com>
2026-04-15 17:51:29 +0800

    chore: fix publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
     alias(libs.plugins.dokka)
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.17.0"
-    id("com.vanniktech.maven.publish") version "0.34.0"
+    id("com.vanniktech.maven.publish") version "0.36.0"
+    signing
 }
 
 repositories {
